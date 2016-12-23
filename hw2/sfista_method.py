2febc6bd1983487364d9c4890e59763f11838b8b
assaf <asafyi@gmail.com>
2016-12-24 00:16:49 +0200

    Use SearchState as Immutable instead of changing x inplace.

--- hw2/sfista_method.py
+++ hw2/sfista_method.py
@@ -42,21 +42,21 @@ class SFISTAMethod(abstract_search_method.SearchMethod):
         super(SFISTAMethod, self).__init__(search_state)
         self._huber_calc = HuberCalculator(mu)
         self._L = L
-        self._y_k = self._state.x
+        self._y_k = self._state.x()
         self._t_k = 1
 
     def step(self):
-        last_x_k = self._state.x
+        last_x_k = self._state.x()
         last_t_k = self._t_k
-        self._state.x = self.get_next_x(self._y_k, self._L)
+        self._state = self._state.move_to_x(self.get_next_x(self._y_k, self._L))
         self._t_k = self.get_next_t(self._t_k)
-        self._y_k = self.get_next_y(self._state.x, last_x_k, self._t_k, last_t_k)
+        self._y_k = self.get_next_y(self._state.x(), last_x_k, self._t_k, last_t_k)
 
     def get_next_x(self, y, L):
         return project_into_simplex(y - 1/L * self.grad_f(y))
 
     def grad_f(self, x):
-        return sum(self._huber_calc.huber_derivative(x, self._state.A[i,:], self._state.b[i]) for i in range(1, self._state.A.shape[0]))
+        return sum(self._huber_calc.huber_derivative(x, self._state.A()[i,:], self._state.b[i]) for i in range(1, self._state.A().shape[0]))
 
     def get_next_t(self, current_t):
         return (1 + (1 + 4 * (current_t ** 2)) ** 0.5) / 2
@@ -65,4 +65,4 @@ class SFISTAMethod(abstract_search_method.SearchMethod):
         return current_x + (current_t - 1) / new_t * (current_x - last_x)
 
     def smoothed_f(self, x):
-        return sum(self._huber_calc.huber(np.dot(self._state.A[i,:], x) - self._state.b[i]) for i in range(1, self._state.A.shape[0]))
+        return sum(self._huber_calc.huber(np.dot(self._state.A()[i,:], x) - self._state.b[i]) for i in range(1, self._state.A().shape[0]))
